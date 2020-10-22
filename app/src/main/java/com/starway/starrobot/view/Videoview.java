1caa91321d9545598a742ef47fdd86b8da87e06c
ha293 <2932669320@qq.com>
2020-10-22 21:20:00 +0800

    创建Videoview activity

---
+++ app/src/main/java/com/starway/starrobot/view/Videoview.java
@@ -0,0 +1,15 @@
+package com.starway.starrobot.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.starway.starrobot.R;
+
+public class Videoview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videoview);
+    }
+}
